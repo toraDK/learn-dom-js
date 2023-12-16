d309a5a9192d65c1677eb2a7773d29e27a95e945
tora180405 <toradigda@gmail.com>
2023-12-16 14:21:21 +0700

    add file

---
+++ dom-manipulation1/script.js
@@ -0,0 +1,5 @@
+const jududl = document.getElementById('judul');
+jududl.innerHTML = 'haloha';
+
+const sectionA = document.querySelector('section#a');
+sectionA.innerHTML = '<div><p>paragraf1</p></div>';
