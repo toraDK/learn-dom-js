ac7222e1a5bfc745fc8cce7ad08cb7f278d7c4e5
tora180405 <toradigda@gmail.com>
2023-12-22 21:54:26 +0700

    edit file

--- suwitjawa/script.js
+++ suwitjawa/script.js
@@ -12,6 +12,24 @@ function getHasil(comp, player) {
     if(player == 'semut') return (comp == 'orang') ? 'KALAH' : 'MENANG!';
 }
 
+function putar() {
+    const imgComputer = document.querySelector('.img-komputer');
+    const gambar = ['gajah', 'semut', 'orang'];
+    let i = 0;
+    const waktuMulai = new Date().getTime();
+    setInterval(function () {
+        if (new Date().getTime() - waktuMulai > 1000) {
+            clearInterval;
+            return;
+        }
+        imgComputer.setAttribute('src', 'img/' + gambar[i++] + '.png');
+        if (i == gambar.length) {
+            i = 0; 
+        }
+    }, 100);
+}
+
+
 const pilihan = document.querySelectorAll('li img');
 pilihan.forEach(function (pil) {
     pil.addEventListener('click', function () {
@@ -19,17 +37,18 @@ pilihan.forEach(function (pil) {
     const pilihanPlayer = pil.className;
     const hasil = getHasil(pilihanComputer, pilihanPlayer);
 
-    const imgComputer = document.querySelector('.img-komputer');
-    imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+    putar();
 
-    const info = document.querySelector('.info');
-    info.innerHTML = hasil;
+    setTimeout(function () {
+        const imgComputer = document.querySelector('.img-komputer');
+        imgComputer.setAttribute('src', 'img/' + pilihanComputer + '.png');
+
+        const info = document.querySelector('.info');
+        info.innerHTML = hasil;
+    }, 1000);
+
     });
 });
-
-
-
-
 
 
 
