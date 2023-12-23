a34b26e330003075d3c8ac495e760fff59574545
tora180405 <toradigda@gmail.com>
2023-12-23 15:24:48 +0700

    edit file

--- dom-traversal/script.js
+++ dom-traversal/script.js
@@ -1,5 +1,15 @@
-const x = document.querySelector('.close');
+// const x = document.querySelector('.close');
 
-x.addEventListener('click', function () {
-    document.querySelector('.card').style.display = 'none';
-});
+// x.addEventListener('click', function () {
+//     document.querySelector('.card').style.display = 'none';
+// });
+
+
+//DOM TRAVESAL
+const close = document.querySelectorAll('.close');
+
+for(let i = 0; i < close.length; i++){
+    close[i].addEventListener('click', function () {
+        close[i].parentElement.style.display = 'none';
+    });
+}
